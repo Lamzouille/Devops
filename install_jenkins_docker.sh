6e9b6eae924596ac72742023ab2ec70d757b3f26
Lamzouille <sylvain.thirard@gmail.com>
2020-02-19 15:45:12 +0100

    Checkstyle

--- install_jenkins_docker.sh
+++ install_jenkins_docker.sh
@@ -6,31 +6,29 @@
 # Check prerequisities
 
 f_logMsg(){
+  level=$1
+  msg=$2
 
-	level=$1
-	msg=$2
+  RED="\033[31m"
+  GREEN="\033[32m"
+  YELLOW="\033[33m"
+  BLUE="\033[34m"
+  BLACK="\033[30m"
+  DELIM_FIN="\033[m"
+  _date=$(date '+%d/%m/%Y - %T')
 
-	RED="\033[31m"
-	GREEN="\033[32m"
-	YELLOW="\033[33m"
-	BLUE="\033[34m"
-	BLACK="\033[30m"
-	DELIM_FIN="\033[m"
-	_date=$(date '+%d/%m/%Y - %T')
+  if [ $level == "ERROR" ]; then
+    COLOR="${RED}"
+  elif [ $level == "DEBUG" ]; then
+    COLOR="${BLUE}"
+  elif [ $level == "SUCCESS" ]; then
+    COLOR="${GREEN}"
+  elif [ $level == "WARNING" ]; then
+    COLOR="${YELLOW}"
+  else COLOR="${WHITE}"
+  fi
 
-
-	if [ $level == "ERROR" ]; then
-		COLOR="${RED}"
-	elif [ $level == "DEBUG" ]; then
-		COLOR="${BLUE}"
-	elif [ $level == "SUCCESS" ]; then
-		COLOR="${GREEN}"
-	elif [ $level == "WARNING" ]; then
-		COLOR="${YELLOW}"
-	else COLOR="${WHITE}"
-	fi
-
-	echo -e "${COLOR}""["$level"] [${_date}] - ""${msg}""${DELIM_FIN}"
+  echo -e "${COLOR}""["$level"] [${_date}] - ""${msg}""${DELIM_FIN}"
 }
 
 # Check if docker is installed
